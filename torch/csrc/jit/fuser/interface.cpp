7e5b077ffa4a47a586bd1fc6cb946eb880aad7f8
Mike Ruberry <mruberry@devfair044.maas>
2019-11-17 00:24:39 -0800

    sample jitted contiguous float kernel

--- torch/csrc/jit/fuser/interface.cpp
+++ torch/csrc/jit/fuser/interface.cpp
@@ -137,11 +137,93 @@ bool validateNode(const Node* const node, const bool dbg = false) {
 
 } // namespace
 
+// Signature of the generated function.
+typedef int (*Func)(void);
+typedef void (*addFunc)(unsigned, float*, float*, float*);
+
+using namespace asmjit;
+using namespace asmjit::x86;
 
 bool cpuMergeNodeWithFusionGroup(const Node* const node, Node* fusion_group) {
   #if FUSER_DEBUG
     std::cout << "cpuMergeNodeWithFusionGroup" << std::endl;
   #endif // FUSER_DEBUG
+
+  JitRuntime rt;
+
+  CodeHolder code;
+  code.init(rt.codeInfo());
+
+  Assembler a(&code);
+
+  // allocates sample arrays
+  constexpr unsigned array_size = 32;
+  float* out = (float*)malloc(sizeof(float) * array_size);
+  float* lhs = (float*)malloc(sizeof(float) * array_size);
+  float* rhs = (float*)malloc(sizeof(float) * array_size);
+
+  // initializes sample arrays
+  for (auto i = decltype(array_size){0}; i < array_size; ++i) {
+    out[i] = 0.f;
+    lhs[i] = 1.f;
+    rhs[i] = 2.f;
+  }
+
+  // Creates fusion (element-by-element contiguous add)
+  Label LoopInc = a.newLabel();
+  Label LoopBody = a.newLabel();
+  Label Exit = a.newLabel();
+
+  // Short-circuits on size == 0
+  a.test(edi, edi);
+  a.je(Exit);
+
+  // Stores # of loop iterations, sets loop counter to zero
+  a.lea(r8d, dword_ptr(rdi, - 1)); // r8 = size - 1
+  a.xor_(eax, eax); // clears eax
+  a.jmp(LoopBody); // do () { } while () loop form
+
+  // Loop incrementer
+  a.bind(LoopInc);
+  a.mov(rax, rdi); // offset = offset + 1
+
+  // Loop body
+  a.bind(LoopBody);
+  a.vmovss(xmm0, dword_ptr(rdx, rax, 2)); // loads lhs[offset]
+  a.vaddss(xmm0, xmm0, dword_ptr(rcx, rax, 2)); // lhs[offset] + rhs[offset]
+  a.lea(rdi, dword_ptr(rax, 1)); // size = offset + 1
+  a.vmovss(dword_ptr(rsi, rax, 2), xmm0); // out[offset] = sum
+
+  // Checks if loop is finished
+  a.cmp(rax, r8); // if offset == size - 1, terminate
+  a.jne(LoopInc);
+
+  // Exit
+  a.bind(Exit);
+  a.ret();
+
+  // Jits the code and stores in function
+  addFunc fn;
+  Error err = rt.add(&fn, &code);
+  if (err) {
+    std::cout << "Error while jitting!" << std::endl;
+  }
+
+  // Computes the add
+  fn(array_size, out, lhs, rhs);
+
+  // Debug prints the output
+  std::cout << "Jitted function result: ";
+
+  for (auto i = decltype(array_size){0}; i < array_size; ++i) {
+    std::cout << out[i] << " ";
+  }
+  std::cout << std::endl;
+
+  // Frees sample arrays
+  std::free(out);
+  std::free(lhs);
+  std::free(rhs);
 
   return false;
 }
@@ -188,7 +270,7 @@ bool mergeNodeWithFusionGroup(const Node* const node, Node* fusion_group) {
   }
 
   const std::shared_ptr<c10::TensorType> out_tensor = output->type()->expect<TensorType>();
-  const auto& fusion_device = *(out_tensor->device());
+  const auto fusion_device = *(out_tensor->device());
 
   #if FUSER_DEBUG
     std::cout << "fusion device " << fusion_device << std::endl;
