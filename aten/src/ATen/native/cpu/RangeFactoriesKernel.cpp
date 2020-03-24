575e6207d61dc5891f14bd54b4d97d2f1d2abb0a
Hong Xu <hong@topbug.net>
2020-03-24 11:40:52 -0700

    Vectorize linspace on CPU.
    Benchmark (gcc 8.3, Debian Buster, turbo off, Release build, Intel(R) Xeon(R) E-2136):
    
    ```python
    import timeit
    for dtype in ('torch.double', 'torch.float'):
        for n, t in [(40_000, 200000),
                    (400_000, 20000)]:
            print(f'torch.linspace(0, 10, {n}, dtype={dtype}) for {t} times')
            print(timeit.timeit(f'torch.linspace(0, 10, {n}, dtype={dtype})', setup=f'import torch', number=t))
    ```
    
    Before:
    
    ```
    torch.linspace(0, 10, 40000, dtype=torch.double) for 200000 times
    11.188449680000303
    torch.linspace(0, 10, 400000, dtype=torch.double) for 20000 times
    10.69958164000036
    torch.linspace(0, 10, 40000, dtype=torch.float) for 200000 times
    11.296819276999486
    torch.linspace(0, 10, 400000, dtype=torch.float) for 20000 times
    10.829777259000366
    ```
    
    After:
    
    ```
    torch.linspace(0, 10, 40000, dtype=torch.double) for 200000 times
    3.704719146999196
    torch.linspace(0, 10, 400000, dtype=torch.double) for 20000 times
    3.0970425030000115
    torch.linspace(0, 10, 40000, dtype=torch.float) for 200000 times
    3.9462350260000676
    torch.linspace(0, 10, 400000, dtype=torch.float) for 20000 times
    3.4302567130007446
    ```
    
    ghstack-source-id: a6a6da73734a0df220d1af0d11517c8c3cfbf4b8
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27957

---
+++ aten/src/ATen/native/cpu/RangeFactoriesKernel.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <ATen/Config.h>
+#include <ATen/Dispatch.h>
+
+#include <ATen/cpu/vec256/vec256.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/native/cpu/Loops.h>
+
+
+namespace at { namespace native {
+namespace {
+
+using namespace vec256;
+
+static void linspace_kernel(TensorIterator& iter, Scalar scalar_start, Scalar scalar_end, int64_t steps) {
+  AT_DISPATCH_ALL_TYPES_AND_COMPLEX(iter.dtype(), "linspace_cpu", [&]() {
+    // step should be of double type for all integral types
+    using step_t = std::conditional_t<std::is_integral<scalar_t>::value, double, scalar_t>;
+    int64_t idx(0);
+    scalar_t start = scalar_start.to<scalar_t>();
+    scalar_t end = scalar_end.to<scalar_t>();
+    step_t step = static_cast<step_t>(end - start) / (steps - 1);
+    cpu_serial_kernel_vec(
+        iter,
+        [start, step, &idx]() -> scalar_t {
+          return start + step * (idx ++);
+        },
+        [start, step, &idx]() -> Vec256<scalar_t> {
+          auto res = Vec256<scalar_t>::arange(start + step * idx, step);
+          idx += Vec256<scalar_t>::size();
+          return res;
+        }
+    );
+  });
+}
+
+} // anonymous namespace
+
+REGISTER_DISPATCH(linspace_stub, &linspace_kernel);
+
+}} // namespace at::native
